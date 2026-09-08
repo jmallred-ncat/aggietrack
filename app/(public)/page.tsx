eeb562024cfd519280d412c4da88627a124749de
James Allred <jmallred@aggies.ncat.edu>
2026-09-08 13:22:32 -0400

    Validate connections are working in preview

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,15 @@
+"use server";
 
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+
+export default async function Home() {
+  const version = await prisma.$queryRaw`SELECT version()` as any;
   return (
     <>
       <h1>AggieTrack</h1>
       <span>Track your Information Technology degree progress at North Carolina A&T State University</span>
+      <span>{version[0].version}</span>
     </>
   );
 }
